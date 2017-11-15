6efdb5567e2fc60dede515f28d0cdda5aa4aeaaa
IWishToBeNormal <32865364+IWishToBeNormal@users.noreply.github.com>
2017-11-15 13:08:27 +0100

    Ferdig feilsjekking, og croping. mangler finpussing og testing

--- Temp new GUI/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ Temp new GUI/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        string bilde = @"C:\temp\temp7.jpg";
+        string bilde = @"C:\temp\temp9.jpg";
         public Form1()
         {
             InitializeComponent();
@@ -27,27 +27,6 @@ namespace WindowsFormsApp1
         {
             PB1.Image = System.Drawing.Image.FromFile(bilde);
             Bitmap bitmap = new Bitmap(bilde);
-
-
-            //lager en grey scale av bildet. kan flyttes når vi finner firkant 90%+ av tida
-            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
-            Bitmap GrayBM = filter.Apply(bitmap);
-            
-
-            // gather statistics
-            ImageStatistics stat = new ImageStatistics(AForge.Imaging.Image.FromFile(bilde));
-            ImageStatistics stat2 = new ImageStatistics(GrayBM);
-            Histogram red = stat.Red;
-            Histogram blue = stat.Blue;
-            Histogram green = stat.Green;
-            Histogram gray = stat2.Gray;
-            MessageBox.Show(red.Mean.ToString() + " Red Mean" + "\n" + blue.Mean.ToString() + " Blue Mean" + "\n" + green.Mean.ToString() + " Green Mean" + "\n" + gray.Mean.ToString() + " Gray Mean");
-            MessageBox.Show(red.Median.ToString() + " Red Median" + "\n" + blue.Median.ToString() + " Blue Median" + "\n" + green.Median.ToString() + " Green Median" + "\n" + gray.Median.ToString() + " Gray Median");
-            MessageBox.Show(gray.Max.ToString() + " Gray Max" + "\n" + gray.Min.ToString() + " Gray Min" );
-            //bruk disse pluss gray.Median for å sjekke feil
-            //må sjekkes med ordentlig bilde og shit
-
-
 
             ColorFiltering colorFilter = new ColorFiltering();
             colorFilter.Red = new IntRange(0, 64);
@@ -64,6 +43,7 @@ namespace WindowsFormsApp1
             BlobCounter blobCounter = new BlobCounter();
 
             blobCounter.FilterBlobs = true;
+            blobCounter.CoupledSizeFiltering = true;
             blobCounter.MinHeight = bitmap.Height /2;
             blobCounter.MinWidth = bitmap.Width /2;
 
@@ -90,10 +70,53 @@ namespace WindowsFormsApp1
             {
                 //croper bildet etter hvilke kanter den finner
                 SimpleQuadrilateralTransformation filter2 = new SimpleQuadrilateralTransformation(corners);
-                PB1.Image = filter2.Apply(bitmap);
+                bitmap = filter2.Apply(bitmap);
+                PB1.Image = bitmap; 
                 MessageBox.Show("temp");
             }
 
+            //fjerner 2% av kantene for å ungå feil.
+            int i1 = bitmap.Height / 100;
+            int i2 = bitmap.Width / 100;
+            Crop Cfilter = new Crop(new Rectangle(i2, i1, bitmap.Width - (2*i2), bitmap.Height - (2*i1)));
+            bitmap = Cfilter.Apply(bitmap);
+            PB1.Image = bitmap;
+
+            //lager en grey scale av bildet. kan flyttes når vi finner firkant 90%+ av tida
+            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+            Bitmap GrayBM = filter.Apply(bitmap);
+
+
+            // gather statistics
+            ImageStatistics stat = new ImageStatistics(AForge.Imaging.Image.FromFile(bilde));
+            ImageStatistics stat2 = new ImageStatistics(GrayBM);
+            Histogram red = stat.Red;
+            Histogram blue = stat.Blue;
+            Histogram green = stat.Green;
+            Histogram gray = stat2.Gray;
+            MessageBox.Show(red.Mean.ToString() + " Red Mean" + "\n" + blue.Mean.ToString() + " Blue Mean" + "\n" + green.Mean.ToString() + " Green Mean" + "\n" + gray.Mean.ToString() + " Gray Mean");
+            MessageBox.Show(red.Median.ToString() + " Red Median" + "\n" + blue.Median.ToString() + " Blue Median" + "\n" + green.Median.ToString() + " Green Median" + "\n" + gray.Median.ToString() + " Gray Median");
+            MessageBox.Show(gray.Max.ToString() + " Gray Max" + "\n" + gray.Min.ToString() + " Gray Min");
+            //bruk disse pluss gray.Median for å sjekke feil
+            //må sjekkes med ordentlig bilde og shit
+            PB1.Image = GrayBM;
+            int median = gray.Median;
+            double mean = gray.Mean;
+            int min = gray.Min;
+            if((mean - min) >= 100)
+            {
+                MessageBox.Show("det er feil!!!!");
+            }
+            else
+            {
+                //MessageBox.Show("riktig!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+            long temp = 0;
+            foreach(int i in gray.Values)
+            {
+                temp++;
+            }
+            MessageBox.Show(temp.ToString());
         }
 
 
