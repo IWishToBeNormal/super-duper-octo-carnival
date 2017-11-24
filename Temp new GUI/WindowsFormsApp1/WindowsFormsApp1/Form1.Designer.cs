44306eacdcc3a2fea270f119a945605df577f45f
IWishToBeNormal <32865364+IWishToBeNormal@users.noreply.github.com>
2017-11-24 12:50:48 +0100

    ny if settning og oppdatert GUI

--- Temp new GUI/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ Temp new GUI/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,14 +30,27 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.PB1 = new System.Windows.Forms.PictureBox();
+            this.redTB = new System.Windows.Forms.TextBox();
+            this.greenTB = new System.Windows.Forms.TextBox();
+            this.blueTB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.GmaxTB = new System.Windows.Forms.TextBox();
+            this.GminTB = new System.Windows.Forms.TextBox();
+            this.GavgTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PB1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(299, 462);
+            this.button1.Location = new System.Drawing.Point(448, 711);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,25 +58,137 @@
             // 
             // PB1
             // 
-            this.PB1.Location = new System.Drawing.Point(12, 12);
+            this.PB1.Location = new System.Drawing.Point(18, 18);
+            this.PB1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PB1.Name = "PB1";
-            this.PB1.Size = new System.Drawing.Size(664, 444);
+            this.PB1.Size = new System.Drawing.Size(996, 683);
             this.PB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.PB1.TabIndex = 1;
             this.PB1.TabStop = false;
             // 
+            // redTB
+            // 
+            this.redTB.Location = new System.Drawing.Point(18, 715);
+            this.redTB.Name = "redTB";
+            this.redTB.Size = new System.Drawing.Size(100, 26);
+            this.redTB.TabIndex = 4;
+            // 
+            // greenTB
+            // 
+            this.greenTB.Location = new System.Drawing.Point(124, 715);
+            this.greenTB.Name = "greenTB";
+            this.greenTB.Size = new System.Drawing.Size(100, 26);
+            this.greenTB.TabIndex = 5;
+            // 
+            // blueTB
+            // 
+            this.blueTB.Location = new System.Drawing.Point(230, 715);
+            this.blueTB.Name = "blueTB";
+            this.blueTB.Size = new System.Drawing.Size(100, 26);
+            this.blueTB.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 750);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Red avg.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(243, 750);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Blue avg.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(137, 750);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Green avg.";
+            // 
+            // GmaxTB
+            // 
+            this.GmaxTB.Location = new System.Drawing.Point(914, 715);
+            this.GmaxTB.Name = "GmaxTB";
+            this.GmaxTB.Size = new System.Drawing.Size(100, 26);
+            this.GmaxTB.TabIndex = 12;
+            // 
+            // GminTB
+            // 
+            this.GminTB.Location = new System.Drawing.Point(808, 715);
+            this.GminTB.Name = "GminTB";
+            this.GminTB.Size = new System.Drawing.Size(100, 26);
+            this.GminTB.TabIndex = 13;
+            // 
+            // GavgTB
+            // 
+            this.GavgTB.Location = new System.Drawing.Point(702, 715);
+            this.GavgTB.Name = "GavgTB";
+            this.GavgTB.Size = new System.Drawing.Size(100, 26);
+            this.GavgTB.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(715, 750);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Grey avg.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(819, 750);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Grey min.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(923, 750);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Grey max.";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(683, 503);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1024, 781);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GavgTB);
+            this.Controls.Add(this.GminTB);
+            this.Controls.Add(this.GmaxTB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.blueTB);
+            this.Controls.Add(this.greenTB);
+            this.Controls.Add(this.redTB);
             this.Controls.Add(this.PB1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.PB1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +196,18 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox PB1;
+        private System.Windows.Forms.TextBox redTB;
+        private System.Windows.Forms.TextBox greenTB;
+        private System.Windows.Forms.TextBox blueTB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox GmaxTB;
+        private System.Windows.Forms.TextBox GminTB;
+        private System.Windows.Forms.TextBox GavgTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Temp new GUI/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ Temp new GUI/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        string bilde = @"C:\temp\temp9.jpg";
+        string bilde = @"C:\temp\temp17.jpg";
         public Form1()
         {
             InitializeComponent();
@@ -72,7 +72,6 @@ namespace WindowsFormsApp1
                 SimpleQuadrilateralTransformation filter2 = new SimpleQuadrilateralTransformation(corners);
                 bitmap = filter2.Apply(bitmap);
                 PB1.Image = bitmap; 
-                MessageBox.Show("temp");
             }
 
             //fjerner 2% av kantene for å ungå feil.
@@ -85,52 +84,42 @@ namespace WindowsFormsApp1
             //lager en grey scale av bildet. kan flyttes når vi finner firkant 90%+ av tida
             Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
             Bitmap GrayBM = filter.Apply(bitmap);
+            PB1.Image = GrayBM;
 
 
             // gather statistics
-            ImageStatistics stat = new ImageStatistics(AForge.Imaging.Image.FromFile(bilde));
+            ImageStatistics stat = new ImageStatistics(bitmap);
             ImageStatistics stat2 = new ImageStatistics(GrayBM);
             Histogram red = stat.Red;
             Histogram blue = stat.Blue;
             Histogram green = stat.Green;
             Histogram gray = stat2.Gray;
-            MessageBox.Show(red.Mean.ToString() + " Red Mean" + "\n" + blue.Mean.ToString() + " Blue Mean" + "\n" + green.Mean.ToString() + " Green Mean" + "\n" + gray.Mean.ToString() + " Gray Mean");
-            MessageBox.Show(red.Median.ToString() + " Red Median" + "\n" + blue.Median.ToString() + " Blue Median" + "\n" + green.Median.ToString() + " Green Median" + "\n" + gray.Median.ToString() + " Gray Median");
-            MessageBox.Show(gray.Max.ToString() + " Gray Max" + "\n" + gray.Min.ToString() + " Gray Min");
-            //bruk disse pluss gray.Median for å sjekke feil
-            //må sjekkes med ordentlig bilde og shit
-            PB1.Image = GrayBM;
-            int median = gray.Median;
-            double mean = gray.Mean;
-            int min = gray.Min;
-            if((mean - min) >= 100)
+
+
+            redTB.Text = Convert.ToString(red.Mean);
+            blueTB.Text = Convert.ToString(blue.Mean);
+            greenTB.Text = Convert.ToString(green.Mean);
+            double Gmean = gray.Mean;
+            GavgTB.Text = Convert.ToString(Gmean);
+            GminTB.Text = Convert.ToString(gray.Min);
+            GmaxTB.Text = Convert.ToString(gray.Max);
+
+
+            int temp = 0;
+            for (int i = 0; i < (Gmean/2); i++)
+            {
+                temp += gray.Values[i];
+            }
+            MessageBox.Show(temp.ToString() + " verdier under " + Convert.ToString(Gmean / 2));
+
+            if (temp > 3)
             {
                 MessageBox.Show("det er feil!!!!");
             }
             else
             {
-                //MessageBox.Show("riktig!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                MessageBox.Show("riktig!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             }
-            long temp = 0;
-            foreach(int i in gray.Values)
-            {
-                temp++;
-            }
-            MessageBox.Show(temp.ToString());
         }
-
-
-        private System.Drawing.Point[] ToPointsArray(List<IntPoint> points)
-        {
-            System.Drawing.Point[] array = new System.Drawing.Point[points.Count];
-
-            for (int i = 0, n = points.Count; i < n; i++)
-            {
-                array[i] = new System.Drawing.Point(points[i].X, points[i].Y);
-            }
-
-            return array;
-        }
-
     }
 }
